032277034b3b9b9dee499c9c41dabdabfa799f99
Maor Swisa <maorswisa@Maors-MacBook-Pro.local>
2022-01-19 15:39:27 +0200

    added decleration without initilization

--- vars.go
+++ vars.go
@@ -1,5 +1,7 @@
 package main
 
 func main() {
-	var name string = "Yonatan" //this is a full variable decleration plus initlization. it specifies var(keyword),variable name and type.
+	var name1 string = "Yonatan" //this is a full variable decleration plus initlization. it specifies var(keyword),variable name and type.
+	var name2 string             //variable decleration without initlization
+
 }
