f875f535eedcbf96b208575d27903a0a52baa751
Maor Swisa <maorswisa@Maors-MacBook-Pro.local>
2022-01-19 15:42:46 +0200

    added multiple short variable declerations

--- vars.go
+++ vars.go
@@ -3,5 +3,7 @@ package main
 func main() {
 	var name1 string = "Yonatan" //this is a full variable decleration plus initlization. it specifies var(keyword),variable name and type.
 	var name2 string             //variable decleration without initlization
-
+	name2 = "Maor"
+	name3 := "Avi"                              //short variable decleration
+	name3, name4 := "Avi wizman", "Yosef Chaim" //short variable assignment to two variables at once. atleast one of them has to be not perviously declared
 }
