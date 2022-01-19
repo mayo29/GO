0e86757a1d51043e08e76a158d761b72b3997cea
Maor Swisa <maorswisa@Maors-MacBook-Pro.local>
2022-01-19 15:48:43 +0200

    added pointer plus modified indentation

--- vars.go
+++ vars.go
@@ -1,9 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
 	var name1 string = "Yonatan" //this is a full variable decleration plus initlization. it specifies var(keyword),variable name and type.
 	var name2 string             //variable decleration without initlization
+
 	name2 = "Maor"
-	name3 := "Avi"                              //short variable decleration
+	name3 := "Avi" //short variable decleration
+
 	name3, name4 := "Avi wizman", "Yosef Chaim" //short variable assignment to two variables at once. atleast one of them has to be not perviously declared
+
+	p := &name3     //p is a pointer that stores name3 adress
+	fmt.Println(*p) //this will print out the value pointed by p which is avi wizman
+
 }
